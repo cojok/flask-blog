dfed2af79f13fb0fc8de581e1bca836a40ab2cf6
Flaviu Cojocariu <cojokka@gmail.com>
2020-06-14 17:36:36 +0200

    added CRUD functionality for posts; added route adn tempalte to see user posts; added pagination on the home and user post pages

--- flaskBlog/routes.py
+++ flaskBlog/routes.py
@@ -1,35 +1,37 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from flaskBlog import app, db, bcrypt, login_manager
-from flaskBlog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskBlog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskBlog.models import User, Post
 
-posts = [
-  {
-    'author': 'cojokka',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'June 11, 2020'
-  },
-  {
-    'author': 'deea',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'June 05, 2020'
-  },
-]
+# posts = [
+  # {
+  #   'author': 'cojokka',
+  #   'title': 'Blog Post 1',
+  #   'content': 'First post content',
+  #   'date_posted': 'June 11, 2020'
+  # },
+  # {
+  #   'author': 'deea',
+  #   'title': 'Blog Post 2',
+  #   'content': 'Second post content',
+  #   'date_posted': 'June 05, 2020'
+  # },
+# ]
 
 @app.route('/')
 @app.route('/home')
 def home():
-   return render_template('home.html', posts=posts)
- 
+  page = request.args.get('page', 1, type=int)
+  posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+  return render_template('home.html', posts=posts)
+
 @app.route('/about')
 def about():
-   return render_template('about.html', title='About Page')
+  return render_template('about.html', title='About Page')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -99,3 +101,63 @@ def account():
   image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
   return render_template('account.html', 
                          title='Account Page', image_file=image_file, form=form)
+  
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+  form = PostForm()
+  if form.validate_on_submit():
+    title = form.title.data
+    content = form.content.data
+    author = current_user
+    new_post = Post(title=title, content=content, author=author)
+    db.session.add(new_post)
+    db.session.commit()
+    flash('Your post has been created!', 'success')
+    return redirect(url_for('home'))
+  return render_template('post_form.html', title='New Post',
+                          form=form, legend='New Post')
+  
+@app.route('/post/<int:post_id>')
+def post(post_id):
+   post = Post.query.get_or_404(post_id)
+   return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/edit", methods=['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+  post = Post.query.get_or_404(post_id)
+  if post.author != current_user:
+      abort(403)
+  form = PostForm()
+  if form.validate_on_submit():
+      post.title = form.title.data
+      post.content = form.content.data
+      db.session.commit()
+      flash('Your post has been updated!', 'success')
+      return redirect(url_for('post', post_id=post.id))
+  elif request.method == 'GET':
+      form.title.data = post.title
+      form.content.data = post.content
+  return render_template('post_form.html', title='Edit Post',
+                          form=form, legend='Edit Post')
+  
+@app.route('/post/<int:post_id>/delete', methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+  post = Post.query.get_or_404(post_id)
+  if post.author != current_user:
+    abort(403)
+  db.session.delete(post)
+  db.session.commit()
+  flash('Your post have been deleted', 'success')
+  return redirect(url_for('home'))
+
+@app.route('/user/<string:username>')
+def user_post(username):
+  page = request.args.get('page', 1, type=int)
+  user = User.query.filter_by(username=username).first_or_404()
+  posts = Post.query.filter_by(author=user)\
+    .order_by(Post.date_posted.desc())\
+      .paginate(page=page, per_page=2)
+  return render_template('user_post.html', posts=posts, user=user)
